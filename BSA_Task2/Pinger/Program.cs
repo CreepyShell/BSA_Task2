8d7ad4ab764d7dfc8f9eeaf3916d15cc46e3aac0
Danil <danil.t404@gmail.com>
2021-06-26 20:42:01 +0300

    Start

---
+++ BSA_Task2/Pinger/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Wrapper;
+using System;
+using System.Threading;
+
+namespace Pinger
+{
+    class Program
+    {
+        private static Random random = new Random();
+        private static RabbitService service = new RabbitService();
+        static void Main(string[] args)
+        {
+            if (service.IsEmptyQueue("pong") && service.IsEmptyQueue("ping"))//в очереди не должно быть больше одного сообщения так же как и
+                service.SendMessageToQueue("startedPong", "pong");//в пинг-понге больше одного мячика
+
+
+            service.ListenQueue("ping", (mess) =>
+            {
+                Console.WriteLine("Message came:" + mess + "--" + DateTime.Now);
+                Thread.Sleep(2500);
+                string sendedMessage = "ping" + random.Next(-1000, -1);
+                service.SendMessageToQueue(sendedMessage, "pong");
+                Console.WriteLine($"Message sended:{sendedMessage}\n");
+            });
+            Console.ReadLine();
+            service.Dispose();
+        }
+    }
+}
---
+++ BSA_Task2/Ponger/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Wrapper;
+using System;
+using System.Threading;
+
+namespace Ponger
+{
+    class Program
+    {
+        private static Random random = new Random();
+        public static RabbitService service = new RabbitService();
+        static void Main(string[] args)
+        {
+            service.ListenQueue("pong", (mess) =>
+            {
+                Console.WriteLine("Message came:" + mess + "--" + DateTime.Now);
+                Thread.Sleep(2500);
+                string sendedMessage = "ping" + random.Next(0, 1000);               
+                service.SendMessageToQueue(sendedMessage, "ping");
+                Console.WriteLine($"Message sended:{sendedMessage}\n");
+            });
+            Console.ReadLine();
+            service.Dispose();
+        }
+
+    }
+}
